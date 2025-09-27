5813c3647ad0db0360b9de14c36170dd15cc8733
cleitonsilva14 <cleiton18ads@gmail.com>
2025-09-26 21:03:32 -0300

    Exemplo de Implementação com InMemoryUserDetailsManager

--- src/main/java/io/localhost/springsecurityintro/config/SecurityConfig.java
+++ src/main/java/io/localhost/springsecurityintro/config/SecurityConfig.java
@@ -8,6 +8,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -43,6 +47,27 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+    }
+
+    // #33 Spring Security | Verify User from Database
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user = User
+                .withDefaultPasswordEncoder()
+                .username("user")
+                .password("1234")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User
+                .withDefaultPasswordEncoder()
+                .username("admin")
+                .password("1234")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
